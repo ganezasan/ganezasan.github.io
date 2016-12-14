2274b5498f9f90ff9ed58e4dd2fa234de08cad62
ganezasan <boss@cheekit.com>
2016-12-14 09:25:52 +0900

    add about page

--- src/containers/about/About.js
+++ src/containers/about/About.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import Markdown from 'react-markdown';
 import './About.css';
+
+function getDefeaultSource() {
+  return [
+    '## Contact',
+    'Email:',
+    '[istaka12@gmail.com](mailto:istaka12@gmail.com)',
+    '',
+    '## About me',
+    '私はエンジニアであり、株式会社チイキットの代表をしています。',
+    'ここ3年ほどはずっとリモートワークをしていて、家かカフェで仕事をしています。',
+    '',
+    'フロントエンドからサーバサイド、アプリ開発まで一貫して開発を行ってきました。',
+    '',
+    'メイン言語はJavaScriptで d3.js を使ったデータビジュアライゼーションの開発を行っています。',
+    'JavaScriptはフロントもサーバもアプリも全ての範囲をカバーできるので最高です。',
+    '',
+    '最近だとReactとd3、Serverless Framework(AWS lambda, API Gateway)をメインで使っています。',
+    '興味がある分野は、データを使ったサービス、統計に関するものです。',
+    '趣味は料理を作ることと、読書です。',
+    '',
+    '[Twitter](https://twitter.com/ganezasan)や[Blog](http://ganeza.hatenablog.com/)もやっているので近況はこちらを見てください'
+  ].join('\n');
+}
 
 function About(props){
   return (
-    <div className="start">
-      <h1>`Hello, I'm Takayuki ito`</h1>
+    <div className="about">
+      <div className="image-box">
+        <img src="/profile.jpg" className="rounded-circle profile" />
+      </div>
+      <Markdown source={getDefeaultSource()} />
     </div>
   );
 }
